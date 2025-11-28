787e17c170a1109659f568a6763bea61b710fc81
고케니 <kenny.kerson@gmail.com>
2025-11-28 22:08:13 +0900

    initial commit

---
+++ src/main/kotlin/com/kenny/atd/AccountTransferDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.kenny.atd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccountTransferDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<AccountTransferDemoApplication>(*args)
+}
